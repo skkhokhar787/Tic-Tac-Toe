b29b873dbd495bc2848d2fe2a9b9445f5a516803
skkhokhar <skkhokhar787@gmail.com>
2026-06-08 15:47:51 +0500

    Refactor checkWinner function to remove unnecessary comment

--- TicTacToeJS.js
+++ TicTacToeJS.js
@@ -80,7 +80,7 @@ const checkWinner = () => {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
         isWinner = true;
-        return; // stop checking further
+        return;
       }
     }
   }
